89f7a20a47d1814efa85e43cbba56943ac9ef853
bravewashere <h.nashar89@gmail.com>
2020-08-07 22:33:07 +0700

    tugas migration

---
+++ database/migrations/2020_08_07_152800_create_like_dislike_jawaban_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikeDislikeJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('like_dislike_jawaban', function (Blueprint $table) {
+            $table->unsignedBigInteger('jawaban_id');
+            $table->foreign('jawaban_id')->references('id')->on('jawaban');
+            $table->unsignedBigInteger('profile_id');
+            $table->foreign('profile_id')->references('id')->on('profile');
+            $table->integer('poin', 11);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('like_dislike_jawaban');
+    }
+}
